563e81f5afd12d8167122f6720aed416e576a725
oryan <oryany12@gmail.com>
2021-10-15 18:32:50 +0300

    testing

--- KeyboardControl.py
+++ KeyboardControl.py
@@ -1,41 +1,53 @@
 from djitellopy import tello
 import KeyPressModule as kp
+import cv2
 from time import sleep
 
 kp.init()
 
 me = tello.Tello()
 me.connect()
+me.streamon()
 print(me.get_battery())
 
 
 def getKeyboardInput():
-    lr, fb, up, yv = 0, 0, 0, 0
+    lr, fb, ud, yv = 0, 0, 0, 0
     speed = 50
 
-    if kp.getKey("LEFT"): lr = -speed
-    elif kp.getKey("RIGHT"): lr = speed
+    if kp.getKey("LEFT"):
+        lr = -speed
+    elif kp.getKey("RIGHT"):
+        lr = speed
 
-    if kp.getKey("UP"): fb = speed
-    elif kp.getKey("LEFT"): fb = -speed
+    if kp.getKey("UP"):
+        fb = speed
+    elif kp.getKey("DOWN"):
+        fb = -speed
 
-    if kp.getKey("w"): ud = speed
-    elif kp.getKey("s"): ud = -speed
+    if kp.getKey("w"):
+        ud = speed
+    elif kp.getKey("s"):
+        ud = -speed
 
-    if kp.getKey("a"): yv = speed
-    elif kp.getKey("d"): yv = -speed
+    if kp.getKey("a"):
+        yv = -speed
+    elif kp.getKey("d"):
+        yv = speed
 
     if kp.getKey("q"): me.land()
     if kp.getKey("e"): me.takeoff()
 
+    if kp.getKey("f"): me.flip("f")
 
+    return [lr, fb, ud, yv]
 
-    return [lr, fb, up, yv]
-
-
-me.takeoff()
 
 while True:
     vals = getKeyboardInput()
     me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
-    sleep(0.05)
+
+    img = me.get_frame_read().frame
+    img = cv2.resize(img, (360, 240))
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
